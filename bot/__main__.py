7ad9ad4baa894432e11d7708aa66bdc828f42789
Lonelyking <98003762+Lonelyking@users.noreply.github.com>
2022-05-09 23:11:34 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/SlamDevs/slam-mirrorbot")
-    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
+    buttons.buildbutton("owner", "https://t.me/sarbudeen786")
+    buttons.buildbutton("Channel", "https://t.me/thanimaibots")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
